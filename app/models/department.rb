c8d1c811fc53b7ed6d6a952a09b92b3cfcb0fcf2
da991319 <castaingregis@yahoo.fr>
2013-11-06 12:59:00 -0700

    add relationship to models

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,4 @@
 class Department < ActiveRecord::Base
+	belongs_to :organization
+	has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
+
+  belongs_to :department
+  belongs_to :organization
 end
